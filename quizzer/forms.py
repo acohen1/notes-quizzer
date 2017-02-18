cb7e5680da7bf8100acdc0a2f5f7df96be287aba
shaoleo1 <shaoleo1@gmail.com>
2017-02-18 15:43:49 -0500

    Change notes charfield to required true

--- quizzer/forms.py
+++ quizzer/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class NotesForm(forms.Form):
-    notes = forms.CharField(required=False)
+    notes = forms.CharField(required=True)
 
     def notes(self):
         notes = self.cleaned_data.get('notes')
