c825d00783ecdafb0a9ef3a126d44a9da0438d98
shaoleo1 <shaoleo1@gmail.com>
2017-02-18 15:35:49 -0500

    Added forms and views code as a base for logic implementation

--- quizzer/forms.py
+++ quizzer/forms.py
@@ -1,2 +1,10 @@
 from django import forms
 
+class NotesForm(forms.Form):
+    notes = forms.CharField(required=False)
+
+    def notes(self):
+        notes = self.cleaned_data.get('notes')
+        """Logic for notes processing"""
+        return notes
+        """Remove above line"""
