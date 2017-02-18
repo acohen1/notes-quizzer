11ec0d171cb8b5b307f95f352a42fb0905da9a8a
Alex Cohen <kookycs@gmail.com>
2017-02-18 18:13:24 -0500

    Finished While Loop

--- quizzer/views.py
+++ quizzer/views.py
@@ -10,6 +10,64 @@ def index(request):
         form = NotesForm(request.POST)
         if form.is_valid():
             notes = form.cleaned_data['notes']
-            """Logic here"""
+            
+            keywords = ["""ADD KEYWORDS HERE"""]
+            questions = ["""HOLDER"""]
+            answers = ["""HOLDER"""]
+
+
+            periodIndex = 0
+            sentence = ""
+            tempString = ""
+            keywordIndex = 0
+            q = ""
+            a = ""
+            while (not notes==""):
+                periodIndex = notes.find(".")
+                sentence = notes[0:periodIndex]
+                for i in keywords:
+                    if sentence.find(i)>=0:
+                        keywordIndex = sentence.find(i)
+                a = sentence[0, keywordIndex-1]
+                answers.append(a)
+                q = sentence[keywordIndex, len(sentence)-1]
+                questions.append(q)
+                notes = notes[len(sentence), len(notes)-1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             return render(request, 'quizzer/questions.html')
     return HttpResponseRedirect(request, 'quizzer/index.html')
